6fe9633e18372f87f72b45f13258fad12c2f5327
Yoss <Yoss@DESKTOP-D88T6H2>
2018-08-16 22:32:02 -0500

    Hola Chicas

--- Hola/src/hola/Hola.java
+++ Hola/src/hola/Hola.java
@@ -16,7 +16,7 @@ public class Hola {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(" Hola Chicos    ");
+        System.out.println(" Hola Chicas    ");
     }
     
 }
